fcee4c18aef774b7bf4ade881338b7879393c2a4
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-02-10 10:10:12 +0000

    feat: add main type definitions

---
+++ packages/lib/data/types.d.ts
@@ -0,0 +1,50 @@
+import { AppealHASCase, AppealS78Case } from 'pins-data-model/src/schemas.d.ts';
+import { Event } from '@microsoft/microsoft-graph-types';
+
+export type AppealCase = AppealHASCase | AppealS78Case;
+export type CalendarEvent = Event;
+
+export interface Inspector {
+	id: string;
+	firstName: string;
+	lastName: string;
+	emailAddress: string;
+	address: Address;
+
+	grade: string;
+	fte: number;
+
+	inspectorManager: boolean;
+	chartingOfficerId: string;
+
+	specialisms: Specialism[];
+	preclusions: Preclusion[];
+}
+
+export interface Address {
+	addressLine1?: string;
+	addressLine2?: string;
+	townCity?: string;
+	county?: string;
+	postcode?: string;
+}
+
+export interface Specialism {
+	name: string;
+	proficiency: 'trained' | 'in-training';
+	validFrom: Date | string;
+}
+
+export type Preclusion = LpaPreclusion | LocationPreclusion | OrganisationPreclusion;
+
+export interface LpaPreclusion {
+	lpaId: string;
+}
+
+export interface LocationPreclusion {
+	postcode: string;
+}
+
+export interface OrganisationPreclusion {
+	organisation: string;
+}
