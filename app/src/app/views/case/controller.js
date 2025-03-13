66cb4155a9415bc0033e273d9cb48859531c8bc6
joshua-wilson-PINS <Joshua.Wilson.AT@planninginspectorate.gov.uk>
2025-03-13 15:00:15 +0000

    Case details page (#13)
    * feat(case details): add case detail and case number text
    
    * chore(view): add fields to case view table and bring inspector data in controller
    
    * chore(view): add seconfd table to new case view
    
    * chore(view): add additional tables to view
    
    * feat: set filters based on selected inspector (#12)
    
    * feat: set filters based on selected inspector
    
    * fix: keep previously selected inspector when changing filters
    
    * chore(view): title chnages and table layout for case detail view
    
    ---------
    
    Co-authored-by: Linus Norton <linusnorton@gmail.com>

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -1,14 +1,20 @@
 import { fetchCases } from '@pins/inspector-programming-poc-lib/data/cases.js';
+import { fetchInspectors } from '@pins/inspector-programming-poc-lib/data/inspectors.js';
 
 export function buildViewCase({ logger }) {
 	return async (req, res) => {
-		logger.info('view case');
+		logger.info(`view case for ID: ${req.params.caseId}`);
 
 		const [caseData] = await fetchCases(1);
-		caseData.id = req.params.caseId;
+		caseData.caseId = req.params.caseId;
+
+		const inspectors = await fetchInspectors(10);
 
 		return res.render('views/case/view.njk', {
-			caseData
+			caseData,
+			inspectors,
+			containerClasses: 'pins-container-wide',
+			title: 'Case Details'
 		});
 	};
 }
