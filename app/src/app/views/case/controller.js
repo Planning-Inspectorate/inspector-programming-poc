704612b444101ed85ecae1058647188e77ca69b9
Linus Norton <linusnorton@gmail.com>
2025-03-13 15:50:57 +0000

    feat: basic map component (#14)
    * feat: basic map component
    
    * fix: router crash

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -1,5 +1,6 @@
 import { fetchCases } from '@pins/inspector-programming-poc-lib/data/cases.js';
 import { fetchInspectors } from '@pins/inspector-programming-poc-lib/data/inspectors.js';
+import { getLatLongForPostcode } from '#util/maps.js';
 
 export function buildViewCase({ logger, config }) {
 	return async (req, res) => {
@@ -10,10 +11,24 @@ export function buildViewCase({ logger, config }) {
 		caseData.caseId = req.params.caseId;
 
 		const inspectors = await fetchInspectors(10);
+		const mapsKey = config.maps.key;
+
+		const casesLatLong = [await getLatLongForPostcode({ key: mapsKey }, caseData.siteAddressPostcode)];
+
+		const inspectorsLatLong = await Promise.all(
+			inspectors
+				.map((c) => c.address?.postcode)
+				.filter(Boolean)
+				.map((postcode) => getLatLongForPostcode({ key: mapsKey }, postcode))
+				.filter(Boolean)
+		);
 
 		return res.render('views/case/view.njk', {
 			caseData,
 			inspectors,
+			apiKey: mapsKey,
+			casesLatLong,
+			inspectorsLatLong,
 			containerClasses: 'pins-container-wide',
 			title: 'Case Details'
 		});
