5662f7a276e8eae98e195741bfb9ff48054445d4
joshua-wilson-PINS <Joshua.Wilson.AT@planninginspectorate.gov.uk>
2025-03-13 13:06:44 +0000

    chore(view): add fields to case view table and bring inspector data in controller

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -1,4 +1,5 @@
 import { fetchCases } from '@pins/inspector-programming-poc-lib/data/cases.js';
+import { fetchInspectors } from '@pins/inspector-programming-poc-lib/data/inspectors.js';
 
 export function buildViewCase({ logger }) {
 	return async (req, res) => {
@@ -7,8 +8,11 @@ export function buildViewCase({ logger }) {
 		const [caseData] = await fetchCases(1);
 		caseData.id = req.params.caseId;
 
+		const inspectors = await fetchInspectors(10);
+
 		return res.render('views/case/view.njk', {
-			caseData
+			caseData,
+			inspectors
 		});
 	};
 }
