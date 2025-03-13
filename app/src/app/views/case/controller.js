8afde3c9647b524451f820c63bb115d5a0d030d0
joshua-wilson-PINS <Joshua.Wilson.AT@planninginspectorate.gov.uk>
2025-03-13 15:00:15 +0000

    Case details page (#13)
    * feat(case details): add case detail and case number text
    
    * chore(view): add fields to case view table and bring inspector data in controller
    
    * chore(view): add seconfd table to new case view
    
    * chore(view): add additional tables to view
    
    * feat: set filters based on selected inspector (#12)
    
    * feat: set filters based on selected inspector
    
    * fix: keep previously selected inspector when changing filters
    
    * chore(view): title chnages and table layout for case detail view
    
    ---------
    
    Co-authored-by: Linus Norton <linusnorton@gmail.com>

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -1,33 +1,19 @@
 import { fetchCases } from '@pins/inspector-programming-poc-lib/data/cases.js';
 import { fetchInspectors } from '@pins/inspector-programming-poc-lib/data/inspectors.js';
-import { getLatLongForPostcode } from '#util/maps.js';
 
 export function buildViewCase({ logger, config }) {
 	return async (req, res) => {
+		logger.info(`view case for ID: ${req.params.caseId}`);
 		logger.info(`view case for ID: ${req.params.caseId}`);
 
 		const [caseData] = await fetchCases(1);
 		caseData.caseId = req.params.caseId;
 
 		const inspectors = await fetchInspectors(10);
-		const mapsKey = config.maps.key;
-
-		const casesLatLong = [await getLatLongForPostcode({ key: mapsKey }, caseData.siteAddressPostcode)];
-
-		const inspectorsLatLong = await Promise.all(
-			inspectors
-				.map((c) => c.address?.postcode)
-				.filter(Boolean)
-				.map((postcode) => getLatLongForPostcode({ key: mapsKey }, postcode))
-				.filter(Boolean)
-		);
 
 		return res.render('views/case/view.njk', {
 			caseData,
 			inspectors,
-			apiKey: mapsKey,
-			casesLatLong,
-			inspectorsLatLong,
 			containerClasses: 'pins-container-wide',
 			title: 'Case Details'
 		});
