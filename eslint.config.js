0f437843ad7c0b4c7a12048895d4fc28c63d16ef
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-17 10:59:00 +0000

    chore: fix linting ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default [
+	{
+		// ignores in its own object to apply to all configs
+		ignores: ['dist/**', 'node_modules/**', '**/*.min*.js', '**/.static/**/*.js']
+	},
 	js.configs.recommended,
 	{
-		ignores: ['dist/**', 'node_modules/**', '**/*.min*.js', '**/static/scripts/app.js'],
 		languageOptions: {
 			ecmaVersion: 2022,
 			sourceType: 'module',
