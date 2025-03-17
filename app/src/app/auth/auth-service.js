f8a825a01c44f8641de5813b3fad3801f8ec778d
Linus Norton <linusnorton@gmail.com>
2025-03-17 10:19:37 +0000

    chore: add scope

--- app/src/app/auth/auth-service.js
+++ app/src/app/auth/auth-service.js
@@ -6,6 +6,7 @@ import { buildMsalConfig } from '#util/auth.js';
 
 const scopes = [
 	'user.read',
+	'Calendars.ReadWrite',
 	'User.ReadBasic.All', // for group membership displayName
 	'GroupMember.Read.All', // for group memberships
 	'Sites.ReadWrite.All' // for sharepoint sites
