84ad5a5365cce5a5a0a6b2e80a66703bf2df1ada
Linus Norton <linusnorton@gmail.com>
2025-03-19 08:24:55 +0000

    feat: fixed data set, actually sort by distance (#24)
    * feat: plan events in calendar
    
    * feat: set body of event
    
    * chore: use permenant data
    
    * chore: add static case list
    
    * fix: fix sort, fix distance calc, fix data, filters going missing when changing sort, all the fixes
    
    * chore: update rpa regions and names to match lat long

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -1,23 +1,19 @@
-import { fetchCases } from '@pins/inspector-programming-poc-lib/data/cases.js';
+import { fetchCase } from '@pins/inspector-programming-poc-lib/data/cases.js';
 import { fetchInspectors } from '@pins/inspector-programming-poc-lib/data/inspectors.js';
-import { getLatLongForPostcode, randomPostcode } from '#util/maps.js';
 
 export function buildViewCase({ logger, config }) {
 	return async (req, res) => {
 		logger.info(`view case for ID: ${req.params.caseId}`);
 
 		const mapsKey = config.maps.key;
-		const [caseData] = await fetchCases(1, undefined, undefined, await randomPostcode({ key: mapsKey }));
-		caseData.caseId = req.params.caseId;
-
+		const caseData = fetchCase(req.params.caseId);
 		const inspectors = await fetchInspectors(config);
-		const caseLatLong = await getLatLongForPostcode({ key: mapsKey }, caseData.siteAddressPostcode);
 
 		return res.render('views/case/view.njk', {
 			caseData,
 			inspectors,
 			apiKey: mapsKey,
-			pins: [caseLatLong],
+			pins: [caseData.siteAddressLatLong],
 			containerClasses: 'pins-container-wide',
 			title: 'Case Details'
 		});
--- app/src/app/views/home/controller.js
+++ app/src/app/views/home/controller.js
@@ -16,9 +16,9 @@ export function buildViewHome({ logger, config }) {
 		const selectedInspector = inspectors.find((i) => req.query.inspector === i.id) || inspectors[0];
 		const filters = req.query.filters || selectedInspector.filters;
 		const sort = req.query.sort || 'age';
-		const inspectorLatLong = { latitude: 53.4808, longitude: 0.0927 };
+		const inspectorLatLong = { latitude: 52.42950704867449, longitude: -1.8996231835728423 };
 		const sortFunc = sort === 'age' ? sortCasesByAge : await createSortByDistance(inspectorLatLong);
-		const cases = await fetchCases(10, filters, sortFunc);
+		const cases = fetchCases(10, filters, sortFunc);
 
 		const pins = cases.map((caseData) => {
 			const { latitude, longitude } = caseData.siteAddressLatLong;
