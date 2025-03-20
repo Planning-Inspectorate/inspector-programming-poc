d8dfaa23b33b9e454efb1ab9b494d3431d93e86b
joshua-wilson-PINS <Joshua.Wilson.AT@planninginspectorate.gov.uk>
2025-03-20 11:29:58 +0000

    fix(case map): fix case map

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -3,17 +3,23 @@ import { fetchInspectors } from '@pins/inspector-programming-poc-lib/data/inspec
 
 export function buildViewCase({ logger, config }) {
 	return async (req, res) => {
-		logger.info(`view case for ID: ${req.params.caseId}`);
-
 		const mapsKey = config.maps.key;
 		const caseData = fetchCase(req.params.caseId);
 		const inspectors = await fetchInspectors(config);
+		const pins = [
+			{
+				lat: caseData.siteAddressLatLong.latitude,
+				long: caseData.siteAddressLatLong.longitude
+			}
+		];
+
+		logger.info(`Case location: ${JSON.stringify(caseData.siteAddressLatLong)}`);
 
 		return res.render('views/case/view.njk', {
 			caseData,
 			inspectors,
 			apiKey: mapsKey,
-			pins: [caseData.siteAddressLatLong],
+			pins,
 			containerClasses: 'pins-container-wide',
 			title: 'Case Details'
 		});
