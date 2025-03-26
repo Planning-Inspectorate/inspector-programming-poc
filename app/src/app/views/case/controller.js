a98ae906048e7504dc5815de610666c469956200
joshua-wilson-PINS <Joshua.Wilson.AT@planninginspectorate.gov.uk>
2025-03-26 14:46:04 +0000

    chore(case map): add inspector location on map

--- app/src/app/views/case/controller.js
+++ app/src/app/views/case/controller.js
@@ -7,11 +7,16 @@ export function buildViewCase({ config }) {
 		const mapsKey = config.maps.key;
 		const caseData = fetchCase(req.params.caseId);
 		const inspectors = await fetchInspectors(config);
+		const inspectorId = req.query.inspectorId;
+		const associatedInspector = inspectors.find((inspector) => inspector.id === inspectorId);
+		const inspectorLatLong = associatedInspector.homeLatLong;
 
 		return res.render('views/case/view.njk', {
 			caseData,
 			inspectors,
+			inspectorId,
 			apiKey: mapsKey,
+			inspectorLatLong,
 			pins: [caseViewModel(caseData)],
 			containerClasses: 'pins-container-wide',
 			title: 'Case Details'
